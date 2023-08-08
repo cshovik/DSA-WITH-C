e83bb56ed855cb278df588776459b4681b0f3467
SHOVIK CHAKRABORTY <113230439+cshovik@users.noreply.github.com>
2023-08-08 23:10:42 +0530

    Update structure &pointer .c

--- structure &pointer .c
+++ structure &pointer .c
@@ -26,3 +26,42 @@ int main()
 
     return 0;
 }
+
+
+//***********************************************************************************************************************************************************
+//structure ponter
+#include<stdio.h>
+#include<string.h>
+
+struct Student{
+    int roll_no;
+    char name[30];//char arry , should use strcpy
+    char branch[40];
+    char batch[40];
+};
+
+struct Student s1, *ptr;
+
+int main()
+{
+    
+   ptr =&s1;
+
+   printf("Enter The Roll no of students\n");
+   scanf("%d", &ptr->roll_no);
+   printf("Enter the name of student\n");
+   scanf("%s", &ptr->name);
+   printf("Enter Branch of Student\n");
+   scanf("%s", &ptr->branch);
+   printf("Enter Batch of Student\n");
+   scanf("%s", &ptr->batch);
+
+   printf("Student details \n");
+
+   printf("Roll No: %d\n", ptr->roll_no);
+   printf("Name: %s\n", ptr->name);
+   printf("Branch: %s\n", ptr->branch);
+   printf("Batch: %s\n", ptr->batch);
+
+    return 0;
+}
