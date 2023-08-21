d61d4e16fbf9a4a401037727d641ed3567f3aaf1
SHOVIK CHAKRABORTY <113230439+cshovik@users.noreply.github.com>
2023-08-22 00:06:15 +0530

    Update 10) Insertion in list .c

--- 10) Insertion in list .c
+++ 10) Insertion in list .c
@@ -268,3 +268,90 @@ Element:66
 Element:77
 Element:56
 */
+
+//************************************************************** insertioin after given node *****************************************************************
+//Insertion  after node
+#include<stdio.h>
+#include<stdlib.h>//for malloc
+
+struct Node{
+  int data;
+  struct Node *next;
+};
+
+//traverse using ptr and print every element
+void linkedListTraversel(struct Node* ptr){
+
+  //print upto ptr become Null
+  while(ptr != NULL){
+    printf("Element: %d\n", ptr->data);
+    ptr = ptr->next;
+  }
+}
+
+//insertion after Node
+struct Node *insertAfterNode(struct Node *head,struct Node *prevNode ,int data){
+  struct Node *ptr =(struct Node *) malloc(sizeof(struct Node *));
+  ptr-> data = data;
+
+  ptr->next = prevNode ->next;
+  prevNode ->next = ptr;
+
+  return head;
+};
+
+
+int main(){
+  struct Node *head;
+  struct Node *second;
+  struct Node *third;
+  struct Node *fourth;
+
+
+
+  //allocate memoryb for nodes in the linked list in heap
+  head = (struct Node*) malloc(sizeof(struct Node));
+  second =(struct Node*) malloc(sizeof(struct Node));
+  third =(struct Node*) malloc(sizeof(struct Node));
+  fourth=(struct Node*) malloc(sizeof(struct Node));
+  
+  //link first and second node
+  head ->data =7;
+  head ->next =second;
+
+  //link second and third node
+  second ->data =11;
+  second ->next =third;
+ 
+  //Link and third and fourth Node
+  third ->data =66;
+  third ->next = fourth; 
+
+  //terminate the node
+  fourth ->data =77;
+  fourth ->next = NULL;
+
+  printf("Linked List before Insertion\n");
+  linkedListTraversel(head);
+
+  head = insertAfterNode(head,second,45);
+  //traversal print after insertion
+  printf("Linked List after Insertion\n");
+  linkedListTraversel(head);
+
+  return 0;
+}
+
+/*output:
+Element:7
+Element:11
+Element:66
+Element:77
+
+Element:7
+Element:11
+Element:45
+Element:66
+Element:77
+
+*/
