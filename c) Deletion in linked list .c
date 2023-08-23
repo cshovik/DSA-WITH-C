d7d2f2bc8c6bd7afc14e8730e4a8416d93bbab29
SHOVIK CHAKRABORTY <113230439+cshovik@users.noreply.github.com>
2023-08-23 11:34:21 +0530

    Update c) Deletion in linked list .c

--- c) Deletion in linked list .c
+++ c) Deletion in linked list .c
@@ -271,3 +271,94 @@ Element:11
 Element:66
 
 */
+//************************************************************************* Deletion of given value ************************************************************
+#include<stdio.h>
+#include<stdlib.h>//for malloc
+
+struct Node{
+  int data;
+  struct Node *next;
+};
+
+//traverse using ptr and print every element
+void linkedListTraversel(struct Node* ptr){
+
+  //print upto ptr become Null
+  while(ptr != NULL){
+    printf("Element: %d\n", ptr->data);
+    ptr = ptr->next;
+  }
+}
+
+//Deletion at particular given value
+struct Node *DeleteAtValue(struct Node *head, int value){
+  struct Node *p = head;
+  struct Node *q= head->next;
+
+  while(q->data !=value && q->next!= NULL){
+     p = p->next;
+     q = q-> next;
+  }
+  if(q ->data == value){
+    p->next = q->next;
+    free(q);
+  }else{
+    printf("Value couldn't find to delete\n");
+  }
+  
+
+  return (head);
+};
+
+
+int main(){
+  struct Node *head;
+  struct Node *second;
+  struct Node *third;
+  struct Node *fourth;
+
+
+
+  //allocate memoryb for nodes in the linked list in heap
+  head = (struct Node*) malloc(sizeof(struct Node));
+  second =(struct Node*) malloc(sizeof(struct Node));
+  third =(struct Node*) malloc(sizeof(struct Node));
+  fourth=(struct Node*) malloc(sizeof(struct Node));
+  
+  //link first and second node
+  head ->data =7;
+  head ->next =second;
+
+  //link second and third node
+  second ->data =11;
+  second ->next =third;
+ 
+  //Link and third and fourth Node
+  third ->data =66;
+  third ->next = fourth; 
+
+  //terminate the node
+  fourth ->data =77;
+  fourth ->next = NULL;
+
+  printf("Linked List before Deletion\n");
+  linkedListTraversel(head);
+
+  head = DeleteAtValue(head,66);//for deleting at particular value
+  
+  printf("Linked List after Deletion\n");
+  linkedListTraversel(head);
+
+  return 0;
+}
+
+/*output:
+Element:7
+Element:11
+Element:66
+Element:77
+
+Element:7
+Element:11
+Element:77
+*/
